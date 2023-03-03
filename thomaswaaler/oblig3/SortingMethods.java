f80917d6fa174244507adea76f8b08621b76e67b
TomWaa <superxgenesis@gmail.com>
2023-03-03 18:42:07 +0100

    Finished Oblig 3

--- thomaswaaler/oblig3/SortingMethods.java
+++ thomaswaaler/oblig3/SortingMethods.java
@@ -4,30 +4,25 @@
  * Task:    Obligatorisk oppgave 3: Test av sorteringsmetoder
  **/
 
-
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Random;
 import java.util.Scanner;
 
-
 class SortingMethods
 {
     // Since we generate random values from 0 to n*2 I can't have an n that will result in n*2 being
-    // bigger than an ints' max value. With some safety margin
+    // bigger than an ints' max value. With some safety margin.
     private static int MAX_ARRAY_LENGTH = 1000000000;
-    private static int MAX_SEQUENTIAL = 100000;
+    private static int MAX_SEQUENTIAL = 100000;         // Insertionsort takes too long for values over this
 
     int n;
     int arr[];
-
-    boolean isRunning; // NOTE: Do I need this?
+    int mergeTempArr[];  // Secondary array used in merge sorting
 
     public SortingMethods()
     {
-        isRunning = true;
-
         System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
         System.out.println("              Sorting-Methods Tester");
         System.out.println("                By Thomas Waaler");
@@ -41,7 +36,7 @@ class SortingMethods
     {
         Scanner s = new Scanner(System.in);
 
-        while (isRunning)
+        while (true)
         {
             System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
 
@@ -58,13 +53,13 @@ class SortingMethods
                 continue;
             }
 
-            System.out.println("[STATUS]: Generating array with length " + n);
+            System.out.print("[STATUS]: Generating array with length " + n + "... ");
             GenerateRandomArray();
-            System.out.println("[STATUS]: DONE");
+            System.out.println("DONE");
 
             System.out.println("\nSelect sorting method:");
             System.out.println("\t1. Insertion Sort\n\t2. Quicksort\n\t3. Merge Sort\n\t4. Radix Sort");
-            System.out.print("> ");
+            System.out.print("INPUT> ");
             int sortMethod = s.nextInt();
             if (sortMethod <= 0 || sortMethod > 4)
             {
@@ -79,7 +74,7 @@ class SortingMethods
 
             System.out.println("\nSelect type of test:");
             System.out.println("\t1. Time sorting\n\t2. Estimate constant C");
-            System.out.print("> ");
+            System.out.print("INPUT> ");
             int testType = s.nextInt();
             System.out.println(""); // For spacing the output
 
@@ -91,19 +86,19 @@ class SortingMethods
                 switch (sortMethod)
                 {
                 case 1:
-                    System.out.println("[STATUS]: Starting Insertion sorting...");
+                    System.out.print("[STATUS]: Starting Insertion sorting... ");
                     InsertionSort();
                     break;
                 case 2:
-                    System.out.println("[STATUS]: Starting Quick sorting...");
+                    System.out.print("[STATUS]: Starting Quick sorting... ");
                     QuickSort();
                     break;
                 case 3:
-                    System.out.println("[STATUS]: Starting Merge sorting...");
+                    System.out.print("[STATUS]: Starting Merge sorting... ");
                     MergeSort();
                     break;
                 case 4:
-                    System.out.println("[STATUS]: Starting Radix sorting...");
+                    System.out.print("[STATUS]: Starting Radix sorting... ");
                     RadixSort();
                     break;
                 default:
@@ -112,18 +107,21 @@ class SortingMethods
                 }
 
                 time = System.currentTimeMillis() - time;
-                System.out.println("[STATUS]: DONE. Time taken: " + time + " ms    " + (time / 1000) + " s");
+                System.out.println("DONE");
+                System.out.println("[STATUS]: Time taken\t" + time + " ms\t" + (time / 1000.0) + " s");
 
+                // Print out the sorted array for small arrays
                 if (n <= 25)
                     System.out.println("\nArray: " + Arrays.toString(arr));
             }
+
             // Estimate constant C
             else if (testType == 2)
             {
                 long totalTime = 0, time;
-                float avgTime;
+                double avgTime;
 
-                System.out.println("[STATUS]: Calculating average runtime...");
+                System.out.print("[STATUS]: Calculating average runtime... ");
                 for (int i = 0; i < 10; i++)
                 {
                     GenerateRandomArray();
@@ -144,29 +142,34 @@ class SortingMethods
                     totalTime += time;
                 }
 
-                avgTime = totalTime / 10.0f;
-                System.out.println("[STATUS]: DONE. Average runtime: " + String.format("%.2f", avgTime) + " ms    " + String.format("%.2f", avgTime / 1000));
+                avgTime = totalTime / 10.0d;
+                System.out.println("DONE");
+                System.out.println("[STATUS]: Time taken     \t" + String.format("%6d", totalTime) + " ms\t" + String.format("%.2f", totalTime / 1000.0) + " s");
+                System.out.println("[STATUS]: Average runtime\t" + String.format("%6.2f", avgTime) + " ms\t" + String.format("%.2f", avgTime / 1000.0) + " s");
 
-                float c = 0.0f;
+                double c = 0.0d;
                 switch (sortMethod)
                 {
-                    case 1: // Insertion
-                        c = avgTime / (n * n);
-                        break;
-                    case 2: // Quick
-                        c = avgTime / (float)(n * Math.log(n));
-                        break;
-                    case 3: // Merge
-                        c = avgTime / (float)(n * Math.log(n));
-                        break;
-                    case 4: // Radix
-                        c = avgTime / n;
-                        break;
+                    // Insertion
+                    case 1: { c = (double)avgTime / (n * n); } break;
+
+                    // Quick
+                    case 2: { c = (double)avgTime / (n * Math.log(n)); } break;
+
+                    // Merge
+                    case 3: { c = (double)avgTime / (n * Math.log(n)); } break;
+
+                    // Radix
+                    case 4: { c = (double)avgTime / n; } break;
+
                     default:
                         System.err.println("[ERROR]: Should never be able to get here");
                         System.exit(-1);
                 }
-                System.out.println("[STATUS]: Estimated value for C: " + String.format("%.2f", c));
+
+                // Printing the value of C with scientific notation since the numbers will be quite low
+                // otherwise we would just get 0 in return
+                System.out.println("[STATUS]: Estimated value for C\t" + c);
             }
             else
             {
@@ -180,7 +183,7 @@ class SortingMethods
             if (s.next().toLowerCase().compareTo("n") == 0)
             {
                 System.out.println("Bye!");
-                isRunning = false;
+                break;
             }
         }
 
@@ -188,7 +191,7 @@ class SortingMethods
         s.close();
     }
 
-    /** Helper function to generate random values in the one dimensional array */
+    // Helper function to generate random values in the one dimensional array
     private void GenerateRandomArray()
     {
         Random r = new Random(System.currentTimeMillis());
@@ -197,14 +200,18 @@ class SortingMethods
             arr[i] = r.nextInt(0, 2 * n);
     }
 
-    // TODO: Comment the code
     private void InsertionSort()
     {
         int current;
 
+        // This loops goes through each element in the array
         for (int i = 0; i < n; i++)
         {
             current = arr[i];
+
+            // This loops goes backwards through the numbers we have already sorted and finds a spot to put the current element.
+            // It will swap the current element with the one to the left if the left element is bigger than the current one.
+            // This will move the current element into the correct spot whilst moving all the big numbers to the right.
             int j = i;
             while (j > 0 && arr[j - 1] > current)
             {
@@ -221,22 +228,23 @@ class SortingMethods
         QuickSort(0, n - 1);
     }
 
-    // TODO: Comment the code
+    // Gotten from "logaritmicSorting.java" by Jan
     private void QuickSort(int min, int max)
     {
         int partitionIndex;
 
+        // End of recursion when min_index has surpassed max_index
         if (max - min > 0)
         {
+            // Partition the array and return the middle element in the partition
             partitionIndex = FindPartition(min, max);
-            // Sort the left partition
-            QuickSort(min, partitionIndex - 1);
-            // Sort the right partition
-            QuickSort(partitionIndex + 1, max);
+
+            QuickSort(min, partitionIndex - 1);   // Sort the left partition
+            QuickSort(partitionIndex + 1, max);   // Sort the right partition
         }
     }
 
-    // TODO: Comment the code
+    // Gotten from "logaritmicSorting.java" by Jan
     private int FindPartition(int min, int max)
     {
         int left, right;
@@ -248,12 +256,15 @@ class SortingMethods
 
         while (left < right)
         {
+            // Increment left until it finds a number that is bigger than the partitionElement
             while (arr[left] <= partitionElement && left < right)
                 left++;
 
+            // Increment right until it find a number that is smaller than the partitionElement
             while (arr[right] > partitionElement)
                 right--;
 
+            // Swap the left and right element only if they haven't reached eachother or surpassed
             if (left < right)
             {
                 tempElement = arr[left];
@@ -262,39 +273,47 @@ class SortingMethods
             }
         }
 
+        // Swap the partitionElement with the last element that is smaller than the partitionElement
         tempElement = arr[min];
         arr[min] = arr[right];
         arr[right] = tempElement;
 
+        // Return the index of the partitionElement
         return right;
     }
 
+    // Function to call the recursive function MergeSort()
     private void MergeSort()
     {
+        mergeTempArr = new int[arr.length];
         MergeSort(0, arr.length - 1);
     }
 
-    // TODO: Comment the code
+    // Gotten from "logarithmicSorting.java" by Jan
     private void MergeSort(int min, int max)
     {
+        // Bottom of the recursion. When there is only one element left
         if (min == max)
             return;
 
+        // Find the middlepoint inbetween min and max
         int mid = (min + max) / 2;
 
-        MergeSort(min, mid);
-        MergeSort(mid + 1, max);
-        Merge(min, mid, max);
+        MergeSort(min, mid);       // MergeSort recursively the left side of the list
+        MergeSort(mid + 1, max);   // MergeSort recursively the right side of the list
+        Merge(min, mid, max);      // Merge the left side list with the right side list in ascending order
     }
 
-    // TODO: Comment the code
+    // Gottem from "logaritmicSorting.java" by Jan
     private void Merge(int min, int mid, int max)
     {
-        int tempArr[] = new int[arr.length];
-
+        // Copy the parts of the list into a temporary array
         for (int i = min; i <= max; i++)
-            tempArr[i] = arr[i];
+            mergeTempArr[i] = arr[i];
 
+        // Merging work by looking at the first index of both lists and compare to see which number is the smallest.
+        // It then takes the smallest number and moves that into our actual array and increments the pointer in that list.
+        // Loops that until both lists are empty, and that has now merged the two lists together in ascending order.
         int left = min;
         int right = mid + 1;
         for (int i = min; i <= max; i++)
@@ -303,42 +322,50 @@ class SortingMethods
             {
                 if (left <= mid)
                 {
-                    if (tempArr[left] > tempArr[right])
-                        arr[i] = tempArr[right++];
+                    if (mergeTempArr[left] > mergeTempArr[right])
+                        arr[i] = mergeTempArr[right++];
                     else
-                        arr[i] = tempArr[left++];
+                        arr[i] = mergeTempArr[left++];
                 }
                 else
-                    arr[i] = tempArr[right++];
+                    arr[i] = mergeTempArr[right++];
             }
             else
-                arr[i] = tempArr[left++];
+                arr[i] = mergeTempArr[left++];
         }
     }
 
-    // TODO: Comment the code
+    // Gotten from "radixSort2.java" by Jan
     private void RadixSort()
     {
+        // Figures out the max amount of digits we will be operating on. Find this by converting the highest number we can get
+        // to a string and then read of the length.
         int maxDigits = String.valueOf(2 * n).length();
         int digitIndex = 1;
 
+        // Create a list of 10 empty queues. One queue for each digit from 0 to 9.
         Queue<Integer>[] queues = (Queue<Integer>[])(new Queue[10]);
         for (int i = 0; i < 10; i++)
             queues[i] = new LinkedList<>();
 
+        // Loop over every digit index of all the numbers
         for (int x = 0; x < maxDigits; x++)
         {
+            // Find the correct queue to add the number in. Numbers that have a 4 in the current digitIndex will go into the
+            // 4th queue and so on.
             for (int i = 0; i < n; i++)
             {
                 int digit = (arr[i] / digitIndex) % 10;
                 queues[digit].add(arr[i]);
             }
 
+            // Empty the queues into the array, now more sorted than the last time.
             int j = 0;
             for (int i = 0; i < 10; i++)
                 while (!queues[i].isEmpty())
                     arr[j++] = (int)queues[i].remove();
 
+            // Move the index to the next 10th place digit
             digitIndex *= 10;
         }
     }
